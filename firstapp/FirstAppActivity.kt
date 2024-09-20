54cad7c5b70e2c2e01f58a569ad31bfc04864911
HackerAndProgrammer <161102679+HackerAndProgrammer@users.noreply.github.com>
2024-09-20 20:18:56 -0300

    Android Aristidevs Course

--- firstapp/FirstAppActivity.kt
+++ firstapp/FirstAppActivity.kt
@@ -2,7 +2,6 @@ package com.example.androidmaster.firstapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -23,16 +22,39 @@ class FirstAppActivity : AppCompatActivity() {
             insets
         }
 
-        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)                 //Access the button component by its id
-        val etName = findViewById<AppCompatEditText>(R.id.etName)                   //Access the editText component by its id
+        val btnStart =
+            findViewById<AppCompatButton>(R.id.btnStart)                 //Access the button component by its id
+        val etName =
+            findViewById<AppCompatEditText>(R.id.etName)                   //Access the editText component by its id
+
+        val name =
+            etName.text.toString()                                       //We convert the value of the editText component to String
 
         btnStart.setOnClickListener {                                               //Is the button clicked?
-            val name = etName.text.toString()                                       //We convert the value of the editText component to String
+            if (name.isNotEmpty()) { //We check if the name is not empty
+                val intent = Intent(
+                    this,
+                    ResultActivity::class.java
+                ) //We create an intent to go to the ResultActivity
+                intent.putExtra(
+                    "EXTRA_NAME",
+                    name
+                )                           //We add the name to the intent
+                startActivity(intent)                                               //We start the activity
+            }
+        }
 
-            if(name.isNotEmpty()){ //We check if the name is not empty
-                val intentOne = Intent(this, ResultActivity::class.java) //We create an intent to go to the ResultActivity
-                intentOne.putExtra("EXTRA_NAME", name)                           //We add the name to the intent
-                startActivity(intentOne)                                               //We start the activity
+        btnStart.setOnClickListener {                       //Is the button clicked?
+            if (name.isNotEmpty()) {                        //We check if the name is not empty
+                val intent = Intent(
+                    this,
+                    TodoActivity::class.java
+                ) //We create an intent to go to the TodoActivity
+                intent.putExtra(
+                    "user_name",
+                    name
+                )                           //We add the name to the intent
+                startActivity(intent)                                               //We start the activity
             }
         }
     }
