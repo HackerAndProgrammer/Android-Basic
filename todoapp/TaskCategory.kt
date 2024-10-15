89a362323c27b0147885525dbb6f71166719df13
HackerAndProgrammer <161102679+HackerAndProgrammer@users.noreply.github.com>
2024-10-15 18:44:27 -0300

    Delete todoapp directory

--- todoapp/TaskCategory.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidmaster.todoapp
-
-sealed class TaskCategory(var isSelected: Boolean = true) {
-    object Personal : TaskCategory() //Personal category object (TaskCategory type)
-    object Business : TaskCategory() //Business category object (TaskCategory type)
-    object Other : TaskCategory()    //Other category object (TaskCategory type)
-}
