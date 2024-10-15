12fc7399bbfea5009b02419a0bb149f9c77a9ab3
HackerAndProgrammer <161102679+HackerAndProgrammer@users.noreply.github.com>
2024-10-15 20:42:36 -0300

    Delete app/kotlin+java/androidmaster directory

--- app/kotlin+java/androidmaster/todoapp/Task.kt
+++
@@ -1,8 +0,0 @@
-package com.example.androidmaster.todoapp
-
-data class Task(
-    val id: Int,  //Unic task id
-    val name: String, //Task name
-    var isSelected: Boolean, //Checks if is selected (true) or not (false)
-    val category: TaskCategory //Category of the Task
-)
