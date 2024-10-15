1e4cddb99b0244a50c23d0198ef1292444d6aa0a
HackerAndProgrammer <161102679+HackerAndProgrammer@users.noreply.github.com>
2024-10-15 19:55:16 -0300

    Delete app/androidmaster directory

--- app/androidmaster/sintaxis/Nulability.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidmaster.sintaxis
-
-fun main(){
-    var name:String? = "Android Development"
-
-    //println(name!!.get(3))                               This doesn't work
-    //print(name?.get(3))                                  This does.
-    print(name?.get(3) ?: "It's a null value")
-}
